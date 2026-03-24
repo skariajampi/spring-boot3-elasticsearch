6bf07858d23b3f9fc858b5e1cee95f62f919dab4
skariaampipparampil <skariajampi@gmail.com>
2026-03-24 19:20:34 +0000

    initial commit

---
+++ src/main/java/org/example/skaria/elasticsearch/springboot3elasticsearch/SpringBoot3ElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package org.example.skaria.elasticsearch.springboot3elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3ElasticsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/skaria/elasticsearch/springboot3elasticsearch/SpringBoot3ElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skaria.elasticsearch.springboot3elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
